985984edd1ad8f2760163532137acbfa0b76a87d
Weslley <121977355+Fakezay@users.noreply.github.com>
2026-06-17 13:01:56 -0300

    Update Android.mk for device 'frost' configuration
    Include subdirectory makefiles and define device for build.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),frost)
+  # Inclui todos os makefiles das subpastas (necessário para os componentes do TWRP)
   include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+  # Define o dispositivo explicitamente para o sistema de build
+  include $(CLEAR_VARS)
+  LOCAL_MODULE := device_frost
+  LOCAL_MODULE_TAGS := optional
+  LOCAL_SRC_FILES := $(shell find $(LOCAL_PATH)/recovery/root -type f)
+  include $(BUILD_PREBUILT)
 endif

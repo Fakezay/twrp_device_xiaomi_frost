9ac757d21e3985587e9d2e18e52e946fd6efb8f2
Weslley <121977355+Fakezay@users.noreply.github.com>
2026-06-17 13:53:38 -0300

    Include recovery/root directory in Android.mk
    Added logic to include recovery/root directory and declare recovery_ramdisk_files module.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
-# Apenas inclua subdiretórios se eles tiverem arquivos Android.mk próprios
-# Não tente copiar arquivos manualmente com BUILD_PREBUILT aqui
-ifeq ($(TARGET_DEVICE),frost)
-  include $(call all-subdir-makefiles,$(LOCAL_PATH))
+# Forçar a inclusão da pasta recovery/root
+ifneq ($(wildcard $(LOCAL_PATH)/recovery/root),)
+    # Aqui estamos declarando que o conteúdo de recovery/root faz parte do target 'recovery'
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := recovery_ramdisk_files
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_CLASS := RECOVERY_RESOURCES
+    include $(BUILD_PHONY_PACKAGE)
 endif
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))

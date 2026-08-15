031a1b00788d41b066079a3a4dc614b5395677f8
Fakezay <fakezaydev@gmail.com>
2026-08-15 23:24:01 +0000

    Enable AVB and set partition size 100663296 for boot hash footer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,14 +39,14 @@ BOARD_KERNEL_PAGESIZE := 4096
 # Recovery FSTAB
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
-# Desativar Android Verified Boot (AVB)
-BOARD_AVB_ENABLE := false
+# Android Verified Boot (AVB)
+BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
-# Configurar chaves de teste para que o bootloader aceite a imagem
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_BOOT_ROLLBACK_INDEX := 1
+BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_BOOT_ADD_HASH_FOOTER_ARGS := --partition_size 100663296
 # Filesystem & Metadata
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_USES_METADATA_PARTITION := true
